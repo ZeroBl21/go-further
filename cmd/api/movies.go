65e421b4ffacf5de99a49338b708abdaabc0caec
Darwin Perdomo <ZeroBl@hotmail.com>
2024-07-21 16:26:11 -0400

    feat(api/): Add custom error responses

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -15,7 +15,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, _ *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -30,8 +30,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	}
 
 	if err := app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil); err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and coult not process your request",
-			http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
