698da8095bb22a74d0f45bb361b75a39b99d2f57
Darwin Perdomo <ZeroBl@hotmail.com>
2024-07-27 11:22:41 -0400

    feat(api/): Add postgres connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -14,6 +16,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -27,9 +32,19 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 5173, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|stating|production)")
 
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
+	logger.Printf("database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -48,4 +63,20 @@ func main() {
 	if err := srv.ListenAndServe(); err != nil {
 		logger.Fatal(err)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/ZeroBl21/go-further
 
 go 1.22.4
+
+require (
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
