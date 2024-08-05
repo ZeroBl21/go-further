359caf90c18f81182a9ede23dec22ff8f5553c36
Darwin Perdomo <ZeroBl@hotmail.com>
2024-08-05 10:22:09 -0400

    feat(api/movies): Handle partial updates, race conditions and change method to patch on updateMovieHandler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,7 +10,7 @@ func (app *application) routes() *http.ServeMux {
 	// Movies
 	router.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
 	router.HandleFunc("POST /v1/movies", app.createMovieHandler)
-	router.HandleFunc("PUT /v1/movies/{id}", app.updateMovieHandler)
+	router.HandleFunc("PATCH /v1/movies/{id}", app.updateMovieHandler)
 	router.HandleFunc("DELETE /v1/movies/{id}", app.deleteMovieHandler)
 
 	return router
