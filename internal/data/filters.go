7bad8b41aa1a62ec910677a5dd79462d6eb0f9da
Darwin Perdomo <ZeroBl@hotmail.com>
2024-08-05 20:17:20 -0400

    fix(data/filters): Fix filter condition check error

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -13,7 +13,7 @@ type Filters struct {
 func ValidateFilters(v *validator.Validator, f Filters) {
 	// Page
 	v.Check(f.Page > 0, "page", "must be greater than zero")
-	v.Check(f.Page <= 0, "page", "must be maximum of 10 million")
+	v.Check(f.Page <= 10_000_000, "page", "must be maximum of 10 million")
 
 	// Page Size
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
