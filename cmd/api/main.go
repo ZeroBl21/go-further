637ad20fecc63d73bc281b18ea97d4cbea3b2694
Darwin Perdomo <ZeroBl@hotmail.com>
2024-08-14 18:23:56 -0400

    feat(api/users): Add send mail to user when is registered

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/ZeroBl21/go-further/internal/data"
 	"github.com/ZeroBl21/go-further/internal/jsonlog"
+	"github.com/ZeroBl21/go-further/internal/mailer"
 	_ "github.com/lib/pq"
 )
 
@@ -29,12 +30,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -60,6 +69,29 @@ func main() {
 	flag.IntVar(&cfg.limiting.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiting.enabled, "limiter-enabled", true, "Enable rate limiter")
 
+	// SMTP
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+
+	flag.StringVar(
+		&cfg.smtp.username,
+		"smtp-username",
+		os.Getenv("GREENLIGHT_SMTP_USERNAME"),
+		"SMTP username",
+	)
+	flag.StringVar(
+		&cfg.smtp.password,
+		"smtp-password",
+		os.Getenv("GREENLIGHT_SMTP_PASSWORD"),
+		"SMTP password",
+	)
+	flag.StringVar(
+		&cfg.smtp.sender,
+		"smtp-sender",
+		"Greenlight <no-reply@greenlight.alexedwards.net>",
+		"SMTP sender",
+	)
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, slog.LevelInfo)
@@ -76,6 +108,13 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(
+			cfg.smtp.host,
+			cfg.smtp.port,
+			cfg.smtp.username,
+			cfg.smtp.password,
+			cfg.smtp.sender,
+		),
 	}
 
 	if err := app.serve(); err != nil {
