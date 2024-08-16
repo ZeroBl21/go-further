e2ed24f5da72cbd14d33788144c089eeb9a0c183
Darwin Perdomo <ZeroBl@hotmail.com>
2024-08-15 23:35:34 -0400

    feat(api/users): Send a email with activation token

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/ZeroBl21/go-further/internal/data"
 	"github.com/ZeroBl21/go-further/internal/validator"
@@ -48,15 +49,26 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
-		err := app.mailer.Send(user.Email, "user_welcome.html", user)
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+
+		err := app.mailer.Send(user.Email, "user_welcome.html", data)
 		if err != nil {
 			app.logger.Error(err, nil)
 			return
 		}
 	})
 
-	err := app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
