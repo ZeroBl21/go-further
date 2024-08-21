16eecdc4ad86b03cd652ef90c442f5e2da3556ef
Darwin Perdomo <ZeroBl@hotmail.com>
2024-08-21 14:07:57 -0400

    feat(users/): Grating movies:read permission when the user registers

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 type Permissions []string
@@ -56,4 +58,20 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	}
 
 	return permissions, nil
+}
+
+// Add the provided permissions codes for a specific user.
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+	INSERT INTO users_permissions
+	SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	args := []any{userID, pq.Array(codes)}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, args...)
+
+	return err
 }
