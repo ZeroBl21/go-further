b7c0709beb595f3e90c16a037640b88637ce613a
Darwin Perdomo <ZeroBl@hotmail.com>
2024-08-23 16:40:21 -0400

    feat(middleware/): Add preflight CORS request handling

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -201,6 +201,7 @@ func (app *application) requirePermissions(code string, next http.HandlerFunc) h
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary", "Origin")
+		w.Header().Add("Vary", "Access-Control-Request-Method")
 
 		origin := r.Header.Get("Origin")
 
@@ -208,6 +209,18 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 			for _, trustedOrigin := range app.config.cors.trustedOrigins {
 				if origin == trustedOrigin {
 					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					if r.Method == http.MethodOptions &&
+						r.Header.Get("Access-Control-Request-Method") != "" {
+
+						w.Header().
+							Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().
+							Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+						w.WriteHeader(http.StatusOK)
+						return
+					}
 				}
 			}
 		}
